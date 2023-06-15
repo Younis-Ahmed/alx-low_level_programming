4c6b60f2741b588e4dc892e50a6634d25f555af9
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-06-14 22:17:12 -0700

    puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: prints a string to the standard error
+ *
+ * Return: Always 1(Error)
+ */
+
+
+int main(void)
+{
+char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fprintf(stderr,  "%s", err);
+
+return (1);
+}
