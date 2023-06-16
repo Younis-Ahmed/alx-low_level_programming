9ab4f6dba3d0a13ed873d43aa7c825b83f57b1b0
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-06-16 02:42:21 -0700

    ifs and else

---
+++ 0x01-variables_if_else_while/4-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Function that will print the alphabet using putchar
+*Return: 0 SUCCES
+*/
+int main(void)
+{
+int i;
+for (i = 97; i <= 122; ++i)
+i == 113 || i == 101 ? i : putchar(i);
+putchar('\n');
+return (0);
+}
