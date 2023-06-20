70237b02ab8f1072790d55ca31eee5f1fe68525d
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-06-20 01:36:53 -0700

    debug

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* _isdigit - checks for char
-* Return: true or false
-* @c: char param
-*/
+ * _isdigit - checks for char
+ * Return: true or false
+ * @c: char param
+ */
 int _isdigit(int c)
 {
-return (c >= 48 && c <= 57);
+	return (c >= 48 && c <= 57);
 }
