cb9c26fa67e8b493b0115b5cf37b1ea9c1b84fdc
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-06-20 00:07:41 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_last_digit - return last digit
+ * @n: num
+ *
+ * Description: return last num
+ * Return: last digits
+ */
+int print_last_digit(int n)
+{
+	int r;
+	if (n < 0)
+	{
+		r = -1 * (n % 10);
+		_putchar(r + '0');
+		return (r);
+	}
+	else
+	{
+		r = n % 10;
+		_putchar(r + '0');
+		return (r);
+	}
+}
