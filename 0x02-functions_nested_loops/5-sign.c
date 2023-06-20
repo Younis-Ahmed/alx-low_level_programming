a66ff6840cc9bc8e60a64273a732b3f0ede2c9cf
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-06-20 00:07:41 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_sign - checks for n range
+ * @n: number
+ *
+ *Description: This is a function to check n range
+ * Return: 1 if n > 0 // 0 if n ==0 // -1 if n < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
