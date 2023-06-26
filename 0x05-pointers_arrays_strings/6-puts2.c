d42cfeb6014a9d3930211af0e0b0085802a0edf9
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-06-25 23:04:38 -0700

    mandatory task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - even string
+ * @str: first param
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		i % 2 == 0 ? _putchar(str[i]) : i;
+	}
+	_putchar('\n');
+}
