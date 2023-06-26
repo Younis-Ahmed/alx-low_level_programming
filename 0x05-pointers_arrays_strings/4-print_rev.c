62f515cd3832c5ffd183ad9cc9627b2f05f1e93c
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-06-25 23:04:38 -0700

    mandatory task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - string in reverse
+ * @s: string to reverse
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int count, i;
+	count = 0;
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		++count;
+	}
+
+
+	for (i = count - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
