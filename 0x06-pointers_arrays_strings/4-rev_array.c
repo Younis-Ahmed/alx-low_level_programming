cbf7ae94ebb9ea9b3668726a6ae569995310ee59
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-06-29 00:24:14 -0700

    advanced done

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
-* reverse_array - reverse array
-* @a: array to reverse
-* @n: n of array
-* Return: void
-*/
+ * reverse_array - reverse array
+ * @a: array to reverse
+ * @n: n of array
+ * Return: void
+ */
 void reverse_array(int *a, int n)
 {
-int i,  mid, temp;
-temp = 0;
-mid = n / 2;
+	int i,  mid, temp;
+	temp = 0;
+	mid = n / 2;
 
-for (i = 0; i < mid; i++)
-{
-temp = a[i];
-a[i] = a[n - i - 1];
-a[n - i - 1] = temp;
-}
+	for (i = 0; i < mid; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
 }
