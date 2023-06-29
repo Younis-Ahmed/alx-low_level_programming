9b1f26af8b1a5a27d3d9bc9ae18fcaf73a2e4420
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-06-29 00:24:14 -0700

    advanced done

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
-* string_toupper - convert string to uppercase
-* @str: string to convert
-* Return: char (upper)
-*/
+ * string_toupper - convert string to uppercase
+ * @str: string to convert
+ * Return: char (upper)
+ */
 char *string_toupper(char *str)
 {
-int i;
-for (i = 0; str[i] != '\0'; i++)
-{
-if (str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] = str[i] - 32;
-}
-}
-return (str);
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
 }
