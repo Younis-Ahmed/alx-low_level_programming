3c99346fb6421bcf642a79fbcdc52f4a47e784f9
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-07-05 03:29:16 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _sqrt - square number
+ * @n: first param
+ * @i: second param
+ * Return: sqrt
+ */
+int _sqrt(int n, int i)
+{
+	int square = i * i;
+
+	if (square > n)
+	{
+		return (-1);
+	}
+	else if (square == n)
+	{
+		return (i);
+	}
+	return (_sqrt(n, i + 1));
+}
+
+/**
+ *_sqrt_recursion - natural sqrt of num
+ * @n: num
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
