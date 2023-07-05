6abe44a407d7af366d3c48b61fdfc43de0ea4182
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-07-05 03:41:36 -0700

    updated proto on 100-is_palindrome | advanced done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+
 /**
 * is_palindrome - returns true if the given string is a palindrome
 *@s: string to check
