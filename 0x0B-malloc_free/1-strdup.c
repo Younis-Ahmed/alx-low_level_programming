16549a410a94c56a5b0d91f7e3e61e8a86cd5d6a
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-07-10 12:12:00 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strdup - pointer to a newly allocated space in memory, with a copy of string
+ * @str: string to copy
+ *Return: pointer
+ */
+char *_strdup(char *str)
+{
+	char *sub;
+	int i = 0, len;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+	{
+		++len;
+		++i;
+	}
+
+	sub = (char *)malloc(len + 1 * sizeof(char));
+
+	if (sub == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+	{
+		sub[i] = str[i];
+	}
+	sub[len] = '\0';
+	return (sub);
+}
