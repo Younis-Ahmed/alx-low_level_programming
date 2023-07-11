99fb3fa5998766524c5ef82ea9bfc6f00df400d0
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-07-11 02:57:46 -0700

    task4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * free_grid - free 2D array
+ * @grid: grid to free
+ * @height: height of array
+ * Return: pointer to the 2D array of int
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
