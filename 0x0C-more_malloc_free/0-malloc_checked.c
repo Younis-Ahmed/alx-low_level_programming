092ba6bf7ed3d63106a77a32bc468b8c5684774f
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-07-12 21:54:39 -0700

    task1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * malloc_checked - function that allocates memory
+ * @b: bytes of memory
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+
+	void *Pblock;
+
+	Pblock = malloc(b);
+
+	if (Pblock == NULL)
+		exit(98);
+
+	return (Pblock);
+}
