5a5976724dbdaac1ecd58916dce005c6fadf372a
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-07-17 03:07:29 -0700

    task3

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,45 @@
+#include "dog.h"
+/**
+ *new_dog - creates a new dog
+ *@name: name of new dog
+ *@age: age of new dog
+ *@owner: owner of new dog
+ *Return: returns NULL in case
+ *of failure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	unsigned int nl, ol, i;
+	dog_t *dog;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	for (nl = 0; name[nl]; nl++)
+		;
+	nl++;
+	dog->name = malloc(nl * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < nl; i++)
+		dog->name[i] = name[i];
+	dog->age = age;
+	for (ol = 0; owner[ol]; ol++)
+		;
+	ol++;
+	dog->owner = malloc(ol * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < ol; i++)
+		dog->owner[i] = owner[i];
+	return (dog);
+}
