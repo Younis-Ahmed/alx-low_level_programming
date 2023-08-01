29f2952723ee99da7e17d38a8d0f2ad8bac2b698
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-08-01 04:01:31 -0700

    betty reformat

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 /**
-* sum_listint - sum of all data (n) of a listint
-* @head: struct
-* Return: int
-*/
+ * sum_listint - sum of all data (n) of a listint
+ * @head: struct
+ * Return: int
+ */
 
 int sum_listint(listint_t *head)
 {
-        int sum = 0;
+	int sum = 0;
 
-        while (head)
-        {
-        sum += head->n;
-        head = head->next;
-        }
-        return (sum);
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
