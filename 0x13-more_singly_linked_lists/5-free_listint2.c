87807da895724ae7ca1035145c69e730357d2587
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-08-01 03:59:53 -0700

    betty reformat

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
 /**
-* free_listint2 - free listint_t list
-* @head: ptr to node
-*/
+ * free_listint2 - free listint_t list
+ * @head: ptr to node
+ */
 
 void free_listint2(listint_t **head)
 {
-        if (!head || !*head)
-                return;
+	if (!head || !*head)
+		return;
 
 
-        free_listint2(&(*head)->next);
-        free(*head);
-        *head = NULL;
+	free_listint2(&(*head)->next);
+	free(*head);
+	*head = NULL;
 }
