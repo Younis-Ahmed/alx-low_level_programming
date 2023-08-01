b20628a7a7592938189e1ed6919aa6cdd18db620
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-08-01 04:01:09 -0700

    betty reformat

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,24 +1,24 @@
 #include "lists.h"
 
 /**
-* pop_listint - delete head node and rtr head node data
-*@head: prt head struct
-*Return: int
-*/
+ * pop_listint - delete head node and rtr head node data
+ *@head: prt head struct
+ *Return: int
+ */
 
 int pop_listint(listint_t **head)
 {
-        listint_t *tmp;
-        int del;
+	listint_t *tmp;
+	int del;
 
-        if (!head || !*(head))
-                return (0);
+	if (!head || !*(head))
+		return (0);
 
-        tmp = *head;
-        del = (*head)->n;
-        *head = tmp->next;
-        free(tmp);
+	tmp = *head;
+	del = (*head)->n;
+	*head = tmp->next;
+	free(tmp);
 
-        return (del);
+	return (del);
 
 }
