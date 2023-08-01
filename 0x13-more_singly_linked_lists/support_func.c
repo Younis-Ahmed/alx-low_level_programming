ba862482fb54103cc1f43ba61fd4032e98ef6cdd
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-08-01 04:03:10 -0700

    betty reformat

--- 0x13-more_singly_linked_lists/support_func.c
+++ 0x13-more_singly_linked_lists/support_func.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 
 /**
-* len - num of elements in list
-*@h: struct
-*Return: unsign int
-*/
+ * len - num of elements in list
+ *@h: struct
+ *Return: unsign int
+ */
 unsigned int len(listint_t *h)
 {
-        unsigned int i = 0;
+	unsigned int i = 0;
 
-        while (h)
-        {
-                ++i;
-                h = h->next;
-        }
-        return (i);
+	while (h)
+	{
+		++i;
+		h = h->next;
+	}
+	return (i);
 }
