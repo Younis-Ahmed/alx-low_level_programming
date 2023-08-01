0415085444f841c07b1ec6f91a2ce433a3f019b3
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-08-01 04:13:23 -0700

    betty reformat

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,5 +1,21 @@
 #include "lists.h"
 
+/**
+ * len - num of elements in list
+ *@h: struct
+ *Return: unsign int
+ */
+unsigned int len(listint_t *h)
+{
+        unsigned int i = 0;
+
+        while (h)
+        {
+                ++i;
+                h = h->next;
+        }
+        return (i);
+}
 
 /**
  *delete_nodeint_at_index - deletes the nodes at (idx) of a listint_t
