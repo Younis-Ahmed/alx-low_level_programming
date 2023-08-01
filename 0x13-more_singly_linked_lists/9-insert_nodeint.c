abfb441a33b0ff2a4d2df46990d07bd9bfc16bd5
Younis Ahmed <23105954+Younis-Ahmed@users.noreply.github.com>
2023-08-01 04:01:39 -0700

    betty reformat

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,61 +1,61 @@
 #include "lists.h"
 
 /**
-* len - num of elements in list
-*@h: struct
-*Return: unsign int
-*/
+ * len - num of elements in list
+ *@h: struct
+ *Return: unsign int
+ */
 unsigned int len(listint_t *h)
 {
-        unsigned int i = 0;
+	unsigned int i = 0;
 
-        while (h)
-        {
-                ++i;
-                h = h->next;
-                }
-                return (i);
+	while (h)
+	{
+		++i;
+		h = h->next;
+	}
+	return (i);
 }
 /**
-* insert_nodeint_at_index - func that insert new node at a given position
-*@head: struct
-*@idx: index where new node should be added
-*@n: number to add inside the node
-*Return: struct or null
-*/
+ * insert_nodeint_at_index - func that insert new node at a given position
+ *@head: struct
+ *@idx: index where new node should be added
+ *@n: number to add inside the node
+ *Return: struct or null
+ */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-        listint_t *new_node, *tmp;
-        unsigned int i = 0;
+	listint_t *new_node, *tmp;
+	unsigned int i = 0;
 
-        if ((!(*head) && idx != 0) || idx > len(*head))
-                return (NULL);
+	if ((!(*head) && idx != 0) || idx > len(*head))
+		return (NULL);
 
-        new_node = malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
 
-        if (!new_node)
-                return (NULL);
+	if (!new_node)
+		return (NULL);
 
-        tmp = *head;
-        new_node->n = n;
-        new_node->next = NULL;
+	tmp = *head;
+	new_node->n = n;
+	new_node->next = NULL;
 
-        if (idx == 0)
-        {
-                new_node->next = *head;
-                *head = new_node;
-                return (new_node);
-        }
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-        while (i < idx - 1)
-        {
-                tmp = tmp->next;
-                i++;
-        }
+	while (i < idx - 1)
+	{
+		tmp = tmp->next;
+		i++;
+	}
 
-        new_node->next = tmp->next;
-        tmp->next = new_node;
+	new_node->next = tmp->next;
+	tmp->next = new_node;
 
-        return (new_node);
+	return (new_node);
 }
